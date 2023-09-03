e4b518a810307e8c75b69e06bf517b1b6078518a
Eric Pratama <ericpratama50@gmail.com>
2023-09-03 16:59:00 +0700

    menambahkan responsive untuk desktop

--- src/script.js
+++ src/script.js
@@ -1,20 +1,19 @@
+// mengambil element form
 const formEl = document.getElementById("form");
+
+// mengambil element input yang berisi feeling
 const feelingInputEl = document.getElementById("feeling-value");
+
+// mengambil div yang memiliki class content
 const contentEl = document.querySelector(".content");
 
-function remove() {
-  setTimeout(() => {
-    formEl.classList.remove("hidden");
-    contentEl.classList.add("hidden");
-  }, 500);
-}
+// mengambil element img
+const imgEl = document.getElementById("img-shower");
 
-function inputValue() {
-  feelingInputEl.value = "happy";
-}
-
+// array yang berisikan image jika feeling value = not happy
 const sadImageArray = ["sad1.jpeg", "sad2.jpeg", "sad3.jpeg", "sad4.jpeg", "sad5.jpeg", "sad6.jpeg", "sad7.jpeg", "sad8.jpeg", "sad9.jpeg", "sad10.jpeg", "sad11.jpeg", "sad12.jpeg", "sad13.jpeg", "sad14.jpeg"];
 
+// array yang berisikan image jika feeling value = happy
 const happyImageArray = [
   "happy1.jpeg",
   "happy2.jpeg",
@@ -23,10 +22,10 @@ const happyImageArray = [
   "happy5.jpeg",
   "happy6.jpeg",
   "happy7.jpeg",
-  "happy8.jpeg",
+  "happy8.png",
   "happy9.jpeg",
   "happy10.jpeg",
-  "happy11.jpeg",
+  "happy11.png",
   "happy12.jpeg",
   "happy13.jpeg",
   "happy14.jpeg",
@@ -35,16 +34,30 @@ const happyImageArray = [
   "happy17.jpeg",
 ];
 
-const imgEl = document.getElementById("img-shower");
+// function yang berfungsi menghilangkan content dan menampilkan form
+function remove() {
+  setTimeout(() => {
+    formEl.classList.remove("hidden");
+    contentEl.classList.add("hidden");
+  }, 500);
+}
 
+// function untuk mengubah feeling value menjadi happy
+function inputValue() {
+  feelingInputEl.value = "happy";
+}
+
+// function untuk merandom happy image
 function getRandomHappyImage() {
   let randomIndex = Math.floor(Math.random() * happyImageArray.length);
 
   let selectedImage = happyImageArray[randomIndex];
 
   imgEl.src = `src/img/${selectedImage}`;
+  imgEl.alt = `${selectedImage}`;
 }
 
+// function untuk merandom sad image
 function getRandomSadImage() {
   let randomIndex = Math.floor(Math.random() * sadImageArray.length);
 
